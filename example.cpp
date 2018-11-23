0e98be71f383fe418ce69061bdcbef88c7c65835
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-11-23 17:14:08 +0100

    init code

---
+++ example.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "lutLDBN.h"
+
+int main()
+{
+ 
+  std::vector<Point> samples;
+  initSamplers();
+ 
+  ldbnBNOT(2048, samples);
+  //2k BN samples
+  for(auto &s: samples)
+    std::cout<< s[0]<<" "<< s[1]<<std::endl;
+  
+  ldbnSTEP(2048, samples);
+  //2k STEP samples
+  for(auto &s: samples)
+    std::cout<< s[0]<<" "<< s[1]<<std::endl;
+  
+  return 0;
+}
